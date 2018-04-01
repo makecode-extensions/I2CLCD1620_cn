8dfa60925ba307d742617893f04e03a4798a1ae6
shaoziyang <shaoziyang@126.com>
2018-04-01 20:47:44 +0800

    升级到 1.2.0

--- main.ts
+++ main.ts
@@ -1,15 +1,14 @@
 /**
- * makecode I2C LCD1602 package for microbit.
- * From microbit/micropython Chinese community.
- * http://www.micropython.org.cn
- */
+* makecode I2C LCD1602 package for microbit.
+* From microbit/micropython Chinese community.
+* http://www.micropython.org.cn
+*/
 
 /**
- * Custom blocks
+ * I2C LCD1602 液晶软件包
  */
 //% weight=100 color=#0fbc11 icon="▀"
 namespace I2C_LCD1602 {
-
     let i2cAddr: number // 0x3F: PCF8574A, 0x27: PCF8574
     let BK: number      // backlight control
     let RS: number      // command/data
@@ -44,12 +43,12 @@ namespace I2C_LCD1602 {
     }
 
     /**
-     * initial LCD, set I2C address. Default is 39/63 for PCF8574/PCF8574A
+     * 初始化 LCD, 设置 I2C 地址。根据芯片不同地址有两种，PCF8574 是 39，PCF8574A 是 63。
      * @param address is i2c address for LCD, eg: 39, 63
      */
-    //% help=functions/show-number
-    //% block
-    export function 液晶初始化(address: number) {
+    //% blockId="I2C_LCD1620_SET_ADDRESS" block="初始化液晶，I2C 地址 %addr"
+    //% weight=100 blockGap=8
+    export function LcdInit(address: number) {
         i2cAddr = address
         BK = 8
         RS = 0
@@ -66,25 +65,31 @@ namespace I2C_LCD1602 {
     }
 
     /**
-     * show a number in LCD at given position
+     * 在液晶的指定位置显示数字
      * @param n is number will be show, eg: 10, 100, 200
      * @param x is LCD column position, eg: 0
      * @param y is LCD row position, eg: 0
      */
-    //% block
-    export function 显示数字(n: number, x: number, y: number): void {
+    //% blockId="I2C_LCD1620_SHOW_NUMBER" block="显示数字 %n|位置 x %x|y %y"
+    //% weight=90 blockGap=8
+    //% x.min=0 x.max=15
+    //% y.min=0 y.max=1
+    export function ShowNumber(n: number, x: number, y: number): void {
         let s = n.toString()
-        显示字符串(s, x, y)
+        ShowString(s, x, y)
     }
 
     /**
-     * show a string in LCD at given position
+     * 在液晶的指定位置显示字符串
      * @param s is string will be show, eg: "Hello"
      * @param x is LCD column position, [0 - 15], eg: 0
      * @param y is LCD row position, [0 - 1], eg: 0
      */
-    //% block
-    export function 显示字符串(s: string, x: number, y: number): void {
+    //% blockId="I2C_LCD1620_SHOW_STRING" block="显示字符串 %s|位置 x %x|y %y"
+    //% weight=90 blockGap=8
+    //% x.min=0 x.max=15
+    //% y.min=0 y.max=1
+    export function ShowString(s: string, x: number, y: number): void {
         let a: number
 
         if (y > 0)
@@ -100,43 +105,48 @@ namespace I2C_LCD1602 {
     }
 
     /**
-     * turn on LCD
+     * 打开液晶显示
      */
-    //% block
-    export function 打开液晶(): void {
+    //% blockId="I2C_LCD1620_ON" block="打开液晶"
+    //% weight=80 blockGap=8
+    export function on(): void {
         cmd(0x0C)
     }
 
     /**
-     * turn off LCD
+     * 关闭液晶显示
      */
-    //% block
-    export function 关闭液晶(): void {
+    //% blockId="I2C_LCD1620_OFF" block="关闭液晶"
+    //% weight=80 blockGap=8
+    export function off(): void {
         cmd(0x08)
     }
 
     /**
-     * clear all display content
+     * 清除液晶上显示的内容
      */
-    //% block
-    export function 清除液晶(): void {
+    //% blockId="I2C_LCD1620_CLEAR" block="清除液晶显示内容"
+    //% weight=75 blockGap=8
+    export function clear(): void {
         cmd(0x01)
     }
 
     /**
-     * turn on LCD backlight
+     * 打开液晶的背光
      */
-    //% block
-    export function 打开液晶背光(): void {
+    //% blockId="I2C_LCD1620_BACKLIGHT_ON" block="打开液晶背光"
+    //% weight=70 blockGap=8
+    export function BacklightOn(): void {
         BK = 8
         dat(0)
     }
 
     /**
-     * turn off LCD backlight
+     * 关闭液晶的背光
      */
-    //% block
-    export function 关闭液晶背光(): void {
+    //% blockId="I2C_LCD1620_BACKLIGHT_OFF" block="关闭液晶背光"
+    //% weight=70 blockGap=8
+    export function BacklightOff(): void {
         BK = 0
         dat(0)
     }
