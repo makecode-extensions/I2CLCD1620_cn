87a278a615dd18577b45e3cbf722da9b1f9b16d4
shaoziyang <shaoziyang@126.com>
2018-11-18 16:03:57 +0800

    修改测试文件

--- test.ts
+++ test.ts
@@ -1,11 +1,8 @@
 let item = 0
-I2C液晶1602.液晶初始化(0)
-I2C液晶1602.显示字符串("Hello", 0, 0)
-basic.pause(1000)
-I2C液晶1602.清除液晶()
-item = 0
+I2C_LCD1602.LcdInit(0)
+I2C_LCD1602.ShowString("Hello", 0, 0)
 basic.forever(() => {
     item += 1
-    I2C液晶1602.显示数字(item, 0, 0)
+    I2C_LCD1602.ShowNumber(item, 0, 1)
     basic.pause(1000)
 })
